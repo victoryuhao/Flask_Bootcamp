43608aaa2772bf787e770b78fdb08949608f73fd
Victor Jiang <vjiang@lendingclub.com>
2019-07-18 17:12:36 -0700

    updated

---
+++ Flask_Plotly/myproject/Scripts/bokeh-script.py
@@ -0,0 +1,12 @@
+#!c:\users\vjiang\documents\github\flask_bootcamp\flask_plotly\myproject\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'bokeh==0.12.5','console_scripts','bokeh'
+__requires__ = 'bokeh==0.12.5'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('bokeh==0.12.5', 'console_scripts', 'bokeh')()
+    )
